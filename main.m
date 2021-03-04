b539a2b4233bf3b4a10a1652d73dcd5f72fc497a
Jaeseung Lee <41176085+tkxkd0159@users.noreply.github.com>
2021-03-04 15:24:28 +0900

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,10 @@
-% import lib.*
-% libtest(3)
-
-lib.libtest(3)
-
-test = lib.MyClass(pi);
+import lib.*
+libtest(3)
+test = MyClass(pi);
 test.roundOff()
+
+
+% lib.libtest(3)
+% 
+% test = lib.MyClass(pi);
+% test.roundOff()
